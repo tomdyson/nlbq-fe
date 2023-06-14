c4ea4e84e329b7eba6a3d27cca3024b12d8a347d
Tom Dyson <tom.dyson@roh.org.uk>
2023-06-14 10:21:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# NLBQ-FE
+
+Example front-end for NLBQ, using Tailwind CSS
+
+`npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch  --minify`
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    // extend: {
+    //   fontFamily: {
+    //     'source-sans-pro': ['source sans pro', 'sans-serif']
+    //   },
+    // },
+  },
+};
